be5bd82be4727b57ac3dbb6264714157d8a5f14d
Alistair English <alistair.e@greenforgelabs.com.au>
2026-02-20 11:34:44 +1000

    2026-02-20 11:34:44

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git add -A && git commit -m "$(date '+%Y-%m-%d %H:%M:%S')" && git pull --rebase && git push
+git add -A && git commit -m "$(date '+%Y-%m-%d %H:%M:%S')" && git pull --rebase origin main && git push
